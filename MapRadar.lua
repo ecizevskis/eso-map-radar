b7eb4274d66d96905b890d2bfc0b03a3c1360792
Edgars Cizevskis <98318868+edgarscizevskis@users.noreply.github.com>
2024-02-27 02:20:03 +0200

    Added calibrator party reader

--- MapRadar.lua
+++ MapRadar.lua
@@ -1,7 +1,22 @@
+-- TODO
+-- Debounce methods for key bindings
+-- Automatic calibration calculation (from map width/height scale to specific 40 pixel count in offset )
+-- Pointer not pointing further than quest or even dissapear if it is nearby
+-- enable ALL pins and start checking what that is 
+-- MapPin type const to name mapping
+-- Saved variable usage (save mode, save radar position)
+-- Configuration page
+-- Icon fading if distance is big (faded as constant if 2x maxDistance ... or gradient fade?)
+-- Fast trakel wayshrines (up to 3 if in same distance +- some threshold)
+-- Closest dolmen
+-- Group via all map
+-- Survey/Treasure if you have map/item (load from LibTreasure)
+-- Test what LINE object is
+-- https://github.com/esoui/esoui/blob/3b9326af2f5946a748be4551bfce41672f084e39/esoui/ingame/map/worldmap.lua#L695
 MapRadar = {
     isOverlayMode = false,
     showPointer = true,
-    showPinCoords = false,
+    showDistance = true,
 
     currentMapWidth = 0,
     currentMapHeight = 0,
@@ -10,88 +25,57 @@ MapRadar = {
 
     positionLabel = {},
 
-    scale = 1 -- This meant to be used and scale param when measuring and calibrating pins on different zones
+    scale = 1, -- This meant to be used and scale param when measuring and calibrating pins on different zones
+
+    showCalibrate = true,
+    showAllPins = false
 }
 
 local UIWidth, UIHeight = GuiRoot:GetDimensions()
 local playerPin = ZO_WorldMap_GetPinManager():GetPlayerPin()
 local pinsPool = ZO_ControlPool:New("PinTemplate", MapRadarContainer, "Pin")
 local pointerPool = ZO_ControlPool:New("PointerTemplate", MapRadarContainer, "Pointer")
-local coordsLabelPool = ZO_ControlPool:New("coordsLabelTemplate", MapRadarContainer, "Coords")
+-- local linePool = ZO_ControlPool:New("ZO_MapKeepLink", container, "Link")
+local distanceLabelPool = ZO_ControlPool:New("LabelTemplate", MapRadarContainer, "Distance")
 
 local activePins = {}
 
+--[[
 local function getVal(obj)
     if obj == nil then
         return "nil"
     end
     return tostring(obj)
 end
+--]]
 
 local function debug(formatString, ...)
     d(zo_strformat(formatString, ...))
 end
 
-function MapRadar_button()
-    local atanRes = math.atan(12, 24)
-
-    -- debug("Atan result: <<1>>", atanRes)
-
-    local currentMapWidth, currentMapHeight = ZO_WorldMap_GetMapDimensions()
-    local x, y, h = GetMapPlayerPosition("player")
-
-    -- :GetCurrentCurvedZoom()
-    -- :GetCurrentNormalizedZoom()
-
-    -- local scale = ZO_WorldMap_GetPanAndZoom():GetCurrentZoom()
-
-    debug("Map dimension: <<1>> <<2>>, Player coords: <<3>> <<4>> <<5>>", currentMapWidth, currentMapHeight, x, y, h)
-    debug("Map curvedZoom: <<1>>, NormalizedZoom: <<2>>", ZO_WorldMap_GetPanAndZoom():GetCurrentCurvedZoom(),
-          ZO_WorldMap_GetPanAndZoom():GetCurrentNormalizedZoom())
-end
-
--- Create control pool
--- FooBar_Bar.barsPool = ZO_ControlPool:New("FooBar", FooBarContainer, "Bar")
-
--- Create new control in pool
--- self.barsPool:ReleaseObject(self.barKey)
-
--- Release from pool
--- 	self.barsPool:ReleaseObject(self.barKey)
-
--- function ZO_MapPin:GetQuestIcon()
---	if self.m_PinTag.isBreadcrumb then
---		return breadcrumbQuestPinTextures[self:GetPinType()]
---	else
---		return questPinTextures[self:GetPinType()]
---	end
--- end
-
--- ZO_MapPin.SelectedAnimation =
--- {
---    texture = "EsoUI/Art/WorldMap/selectedQuestHighlight.dds",
---    duration = LOOP_INDEFINITELY,
---    type = ZO_MapPin.ANIMATION_ALPHA,
--- }
-
--- local orgGetNormalizedPositionFocusZoomAndOffset = ZO_MapPanAndZoom.GetNormalizedPositionFocusZoomAndOffset
--- function ZO_MapPanAndZoom:GetNormalizedPositionFocusZoomAndOffset(normalizedX, normalizedY, useCurrentZoom)
---	if WORLD_MAP_MANAGER:GetMode() ~= MAP_MODE_VOTANS_MINIMAP then
---		return orgGetNormalizedPositionFocusZoomAndOffset(self, normalizedX, normalizedY, useCurrentZoom)
---	else
---		return FocusZoomAndOffset(self, normalizedX, normalizedY)
---	end
--- end
-
 local function IsValidPin(pin)
-    if pin == playerPin or pin:GetPinType() == MAP_PIN_TYPE_DRAGON_IDLE_HEALTHY or pin:GetPinType() == MAP_PIN_TYPE_DRAGON_IDLE_WEAK or
-        pin:GetPinType() == MAP_PIN_TYPE_ACTIVE_COMPANION then
+    local pinType = pin:GetPinType()
+
+    if pin == playerPin -- or pin:GetPinType() == MAP_PIN_TYPE_DRAGON_IDLE_HEALTHY or pin:GetPinType() == MAP_PIN_TYPE_DRAGON_IDLE_WEAK 
+    or pin:IsCompanion() then
         return false
     end
 
+    -- These withouy name might be comming from LIB
+    -- Unaquired quest markers. Need to find you what constant that is or what Is<> method
+    if pinType == 293 -- Unaquired quest markers. Need to find you what constant that is or what Is<> method
+    or pinType == 295 -- Unaquired quest markers. Need to find you what constant that is or what Is<> method
+    -- or pinType == 315 -- chests
+    or pinType == 301 -- zone story quest to grab
+    then
+        return true
+    end
+
     if pin:IsQuest() -- or pin:IsObjective() -- or pin:IsAvAObjective()
-    or pin:IsUnit() -- or pin:IsPOI()
-    -- or pin:IsAssisted() -- or pin:IsMapPing()
+    or pin:IsUnit() -- Player/Group/Companion units
+    -- or pin:IsPOI()
+    -- or pin:IsCompanion()
+    or pin:IsAssisted() -- or pin:IsMapPing()
     -- or pin:IsKillLocation()
     -- or pin:IsWorldEventUnitPin()
     -- or pin:IsZoneStory() or pin:IsSuggestion() -- or pin:IsAreaPin()
@@ -100,18 +84,17 @@ local function IsValidPin(pin)
         return true
     end
 
-    return false
+    return MapRadar.showAllPins
 end
 
 local function GetIcon(pin)
-    -- texture:GetTextureFileName() 
-
-    -- local fileName = pin.backgroundControl:GetTextureFileName()
-    -- local fileName = pin.highlightControl:GetTextureFileName()
-    -- debug(fileName)
 
     if pin:IsUnit() then
-        return "MapRadar/x.dds"
+        -- return "MapRadar/x.dds"
+    end
+
+    if pin:IsCompanion() then
+        return "EsoUI/Art/MapPins/activeCompanion_pin.dds"
     end
 
     if pin:IsUnit() then
@@ -126,18 +109,12 @@ local function GetIcon(pin)
         return pin:GetFastTravelIcons()
     end
 
-    return "EsoUI/Art/MapPins/UI_Worldmap_pin_customDestination.dds"
-end
-
-local function clearPins()
-    -- Clear existing pins
-    for k in pairs(activePins) do
-        activePins[k]:ClearAnchors()
-        activePins[k] = nil
+    local texture = ZO_MapPin.GetStaticPinTexture(pin:GetPinType())
+    if texture ~= nil then
+        return texture
     end
-    pinsPool:ReleaseAllObjects()
-    pointerPool:ReleaseAllObjects()
-    coordsLabelPool:ReleaseAllObjects()
+
+    return "EsoUI/Art/MapPins/UI_Worldmap_pin_customDestination.dds"
 end
 
 local function addPointerToPin(pinTexture)
@@ -150,35 +127,36 @@ local function addPointerToPin(pinTexture)
     pinTexture.pointer = pointerTexture
 end
 
-local function addCoordsToPin(pinTexture)
-    local coordsLabel, labelKey = coordsLabelPool:AcquireObject()
-    coordsLabel:SetAnchor(TOPLEFT, pinTexture, TOPRIGHT)
-    -- coordsLabel:SetColor(unpack({1, 1, 1, 1}))
+local function addDistanceToPin(pinTexture)
+    local label = distanceLabelPool:AcquireObject()
+    label:SetAnchor(TOPLEFT, pinTexture, TOPRIGHT)
 
-    pinTexture.coordsLabel = coordsLabel
+    pinTexture.distanceLabel = label
 end
 -- https://www.codecademy.com/resources/docs/lua/tables
 
 -- https://esoapi.uesp.net/100031/src/ingame/map/mappin.lua.html
 -- https://esodata.uesp.net/100025/src/ingame/map/worldmap.lua.html
 local function registerMapPins()
+    -- Clear existing pins
+    for k in pairs(activePins) do
+        activePins[k]:ClearAnchors()
+        activePins[k] = nil
+    end
+    pinsPool:ReleaseAllObjects()
+    pointerPool:ReleaseAllObjects()
+    distanceLabelPool:ReleaseAllObjects()
+
+    -- Add new pins
     local pins = ZO_WorldMap_GetPinManager():GetActiveObjects()
-
-    -- TODO: create filter methods that return filtered pins 
-
     for pinKey, pin in pairs(pins) do
-        -- debug("Looped pin: <<1>>,  Val: <<2>>", pinKey, getVal(pin))
-
         if IsValidPin(pin) and pin.normalizedX and pin.normalizedY then
             local pinTexture, pinTextureKey = pinsPool:AcquireObject()
-
-            pinTexture.x = pin.normalizedX
-            pinTexture.y = pin.normalizedY
-            pinTexture.pin = pin
             pinTexture:SetTexture(GetIcon(pin))
+            pinTexture.pin = pin
 
-            if MapRadar.showPinCoords then
-                addCoordsToPin(pinTexture)
+            if MapRadar.showDistance then
+                addDistanceToPin(pinTexture)
             end
 
             if MapRadar.showPointer then
@@ -186,31 +164,29 @@ local function registerMapPins()
             end
 
             activePins[pinTextureKey] = pinTexture
-            -- debug("Added pin: <<1>>,  Normalized: <<2>> <<3>>", pinTextureKey, pin.normalizedX, pin.normalizedY)
         end
     end
 end
 
-local function pinReset()
-    local playerZone = GetPlayerActiveZoneName()
-    local playerSubZone = GetPlayerActiveSubzoneName()
-    local zoneName = ZO_WorldMap.zoneName
+-- local function pinReset()
+-- local playerZone = GetPlayerActiveZoneName()
+-- local playerSubZone = GetPlayerActiveSubzoneName()
+-- local zoneName = ZO_WorldMap.zoneName
 
-    -- If map zone name does not match player zone/subzone that means map is just navigated and should not regenerate pins.
-    if zoneName ~= playerZone and zoneName ~= playerSubZone then
-        -- debug("pZone: <<2>>,  pSubZone: <<3>>,  Zone: <<4>>", mapName, playerZone, playerSubZone, zoneName)
+-- If map zone name does not match player zone/subzone that means map is just navigated and should not regenerate pins.
+-- if zoneName ~= playerZone and zoneName ~= playerSubZone then
+-- debug("pZone: <<2>>,  pSubZone: <<3>>,  Zone: <<4>>", mapName, playerZone, playerSubZone, zoneName)
 
-        -- For now let it stay without filter. Surfing map gegenerates pins but closing them back generates them as was
-        -- with this regen it guarantees to regen pins in some map changes where GetPlayerActiveSubzoneName() does not return correct value still
+-- For now let it stay without filter. Surfing map gegenerates pins but closing them back generates them as was
+-- with this regen it guarantees to regen pins in some map changes where GetPlayerActiveSubzoneName() does not return correct value still
 
-        -- return 
-    end
+-- return 
+-- end
 
-    -- d("Pin reset")
-    -- Trigger pin reset
-    clearPins()
-    registerMapPins()
-end
+-- Trigger pin reset
+-- clearPins()
+-- registerMapPins()
+-- end
 
 -- ========================================================================================
 -- pinTexure handling methods
@@ -230,26 +206,36 @@ end
 -- end
 
 local function updatePinTexture(pinTexture, playerX, playerY, heading, curvedZoom)
-    local relative_dx = pinTexture.x - playerX
-    local relative_dy = pinTexture.y - playerY
+    local relative_dx = pinTexture.pin.normalizedX - playerX
+    local relative_dy = pinTexture.pin.normalizedY - playerY
 
     local dx = relative_dx * MapRadar.currentMapWidth * MapRadar.scale
     local dy = relative_dy * MapRadar.currentMapHeight * MapRadar.scale
     local size = 25 -- size is smaller for radar mode and will be bigger for overlay mode
 
     local angle = math.atan2(-dx, -dy) - heading
-    local distance = math.min(MapRadar.maxDistance, math.sqrt(dx ^ 2 + dy ^ 2))
+    local distance = math.sqrt(dx ^ 2 + dy ^ 2)
+    local radarDistance = math.min(MapRadar.maxDistance, distance)
 
     -- recalc coordinates to apply rotation
-    dx = distance * -math.sin(angle)
-    dy = distance * -math.cos(angle)
+    dx = radarDistance * -math.sin(angle)
+    dy = radarDistance * -math.cos(angle)
 
     if pinTexture.pointer ~= nil then
         pinTexture.pointer:SetTextureRotation(angle, 0.5, 1)
+
+        if radarDistance < 64 then
+            pinTexture.pointer:SetDimensions(8, radarDistance)
+        end
+
     end
 
-    if (pinTexture.coordsLabel ~= nil) then
-        pinTexture.coordsLabel:SetText(zo_strformat("<<1>> <<2>>", relative_dx * 10000000, relative_dy * 10000000))
+    if (pinTexture.distanceLabel ~= nil) then
+        pinTexture.distanceLabel:SetText(zo_strformat("<<1>>", distance))
+
+        if MapRadar.showAllPins then
+            pinTexture.distanceLabel:SetText(zo_strformat("<<1>> <<2>>", pinTexture.pin:GetPinType(), MR_PinTypeNames[pinTexture.pin:GetPinType()]))
+        end
     end
 
     -- TODO: need to set translparency/opacity of texture based on distance
@@ -285,6 +271,7 @@ end
 -- Event handlers
 
 -- zoneChangeCheck might not be needed at all
+--[[
 local prevZone = ""
 local prevSubZone = ""
 local function zoneChangeCheck()
@@ -300,8 +287,9 @@ local function zoneChangeCheck()
     debug("ZoneChange: zone: <<1>>, subzone: <<2>>", playerZone, playerSubZone)
 
     -- Trigger pin reset
-    pinReset()
+    registerMapPins()
 end
+--]]
 
 local function mapUpdate()
     local heading = GetPlayerCameraHeading()
@@ -334,7 +322,23 @@ local function mapPinCountCheck()
     -- df("Pin count changed %d", maxn)
 
     prevPinCount = maxn
-    pinReset()
+    registerMapPins()
+end
+
+local function getMapScale()
+    -- For now set very apporx scale
+
+    -- Standard zone
+    if MapRadar.currentMapWidth == 3156 or MapRadar.currentMapWidth == 2752 then
+        return 4.15
+    end
+
+    -- Some DLC middle maps
+    if MapRadar.currentMapWidth == 1945 then
+        return 2.2
+    end
+
+    return 1.1 -- Standard subzone
 end
 
 local function initialize(eventType, addonName)
@@ -354,10 +358,9 @@ local function initialize(eventType, addonName)
     positionLabel:SetColor(unpack({1, 1, 1, 1}))
     MapRadar.positionLabel = positionLabel
 
-    MapRadar.PinReset = function()
-        pinReset()
+    if MapRadar.showCalibrate then
+        MapRadar_InitScaleCalibrator(playerPinTexture)
     end
-    -- MapRadar_InitScaleCalibrator(playerPinTexture)
 
     -- Set mode to radar from start (should be saved to variables later)
     setOverlayMode(MapRadar.isOverlayMode);
@@ -368,10 +371,12 @@ local function initialize(eventType, addonName)
 
     EVENT_MANAGER:RegisterForUpdate("MapRadar_OnUpdate", 30, mapUpdate)
     EVENT_MANAGER:RegisterForUpdate("MapRadar_PinCount", 100, mapPinCountCheck)
+
     CALLBACK_MANAGER:RegisterCallback("OnWorldMapChanged", function()
-        -- d("OnWorldMapChanged")
-        -- pinReset()
-        -- zoneChangeCheck()
+        zo_callLater(function()
+            MapRadar.scale = getMapScale()
+        end, 200)
+
     end)
 end
 
@@ -391,27 +396,27 @@ end)
 -- This is good to trigger pin reset (if quest chnages 1 marker then pin count check does not see difference)
 EVENT_MANAGER:RegisterForEvent("MapRadar", EVENT_QUEST_ADVANCED, function()
     -- debug("EVENT_QUEST_ADVANCED")
-    zo_callLater(pinReset, 200)
+    zo_callLater(registerMapPins, 200)
 end)
 
 EVENT_MANAGER:RegisterForEvent("MapRadar", EVENT_QUEST_COMPLETE, function()
     -- debug("EVENT_QUEST_COMPLETE")
-    zo_callLater(pinReset, 200)
+    zo_callLater(registerMapPins, 200)
 end)
 
 EVENT_MANAGER:RegisterForEvent("MapRadar", EVENT_QUEST_ADDED, function()
     -- debug("EVENT_QUEST_ADDED ")
-    zo_callLater(pinReset, 200)
+    zo_callLater(registerMapPins, 200)
 end)
 
 EVENT_MANAGER:RegisterForEvent("MapRadar", EVENT_QUEST_POSITION_REQUEST_COMPLETE, function()
     -- debug("EVENT_QUEST_POSITION_REQUEST_COMPLETE ")
-    zo_callLater(pinReset, 200)
+    zo_callLater(registerMapPins, 200)
 end)
 
 EVENT_MANAGER:RegisterForEvent("MapRadar", EVENT_QUEST_CONDITION_COUNTER_CHANGED, function()
     -- debug("EVENT_QUEST_CONDITION_COUNTER_CHANGED ")
-    zo_callLater(pinReset, 200)
+    zo_callLater(registerMapPins, 200)
 end)
 
 EVENT_MANAGER:RegisterForEvent("MapRadar", EVENT_ALL_GUI_SCREENS_RESIZED, function()
@@ -427,4 +432,17 @@ ZO_CreateStringId("SI_BINDING_NAME_MAPRADAR_TOGGLE", "Toggle mode")
 -- Hanller for configured hotkey
 function MapRadar_ToggleMode()
     setOverlayMode(not MapRadar.isOverlayMode)
+end
+
+-- ==================================================================================================
+-- Test stuff 
+
+function MapRadar_button()
+    registerMapPins() -- reset all pins placement (for calibration)
+
+    local currentMapWidth, currentMapHeight = ZO_WorldMap_GetMapDimensions()
+    local x, y, h = GetMapPlayerPosition("player")
+
+    debug("Map dimension: <<1>> <<2>>", currentMapWidth, currentMapHeight)
+    debug("Map curvedZoom: <<1>>", ZO_WorldMap_GetPanAndZoom():GetCurrentCurvedZoom() * 1000)
 end
--- ScaleCalibrator.lua
+++ ScaleCalibrator.lua
@@ -1,12 +1,121 @@
 local scaleLabel = {}
+local labelPool = ZO_ControlPool:New("LabelTemplate", MapRadarContainer, "Data")
+
+local ScaleData = {
+    zoneName = {},
+    mapWidth = {},
+    mapHeight = {},
+    curvedZoom = {},
+
+    rel_dx = {},
+    rel_dy = {},
+    map_dx = {},
+    map_dy = {},
+
+    unit1 = {},
+    mrScale = {}
+}
 
 local function setScaleLabel(val)
     MapRadar.scale = MapRadar.scale + val
     scaleLabel:SetText(MapRadar.scale)
-    MapRadar.PinReset()
+end
+
+local function CreateLabel(anchorPoint, anchor, targetAnchorPoint, text)
+    local label, labelKey = labelPool:AcquireObject()
+    label:SetFont("$(BOLD_FONT)|18|outline")
+    label:SetColor(unpack({1, 1, 1, 1}))
+    label:SetAnchor(anchorPoint, anchor, targetAnchorPoint)
+
+    if text ~= nil then
+        label:SetText(text)
+    end
+
+    return label;
+end
+
+local function showCalibrationData(pin)
+    local displayMultiplier = 10000000
+    local measuredMeters = 40
+
+    local playerX, playerY = GetMapPlayerPosition("player")
+    local curvedZoom = ZO_WorldMap_GetPanAndZoom():GetCurrentCurvedZoom()
+    local currentMapWidth, currentMapHeight = ZO_WorldMap_GetMapDimensions()
+
+    -- TODO  Take in account that map can be with different Width and Height (Do calc on paper for getting formulas right!!!)
+    local relative_dx = pin.normalizedX - playerX
+    local relative_dy = pin.normalizedY - playerY
+
+    local map_dx = relative_dx * currentMapWidth
+    local map_dy = relative_dy * currentMapHeight
+
+    local distance = math.sqrt(relative_dx ^ 2 + relative_dy ^ 2)
+
+    local unit1 = distance / measuredMeters -- calculate relative part for 1 meter
+    local unit1k = unit1 * 1000
+
+    local mrScale = 0; -- need to calculate here
+
+    ScaleData.zoneName:SetText(ZO_WorldMap.zoneName)
+    ScaleData.mapWidth:SetText(currentMapWidth)
+    ScaleData.mapHeight:SetText(currentMapHeight)
+    ScaleData.curvedZoom:SetText(curvedZoom)
+    ScaleData.rel_dx:SetText(zo_strformat("<<1>>", relative_dx * displayMultiplier))
+    ScaleData.rel_dy:SetText(zo_strformat("<<1>>", relative_dy * displayMultiplier))
+    ScaleData.map_dx:SetText(map_dx)
+    ScaleData.map_dy:SetText(map_dy)
+    ScaleData.unit1:SetText(unit1)
+    ScaleData.mrScale:SetText(mrScale)
+
+    -- just show actual scale value if it changed
+    setScaleLabel(0)
+end
+
+local function CreateCalibrationDataForm()
+    local dataAnchorControl = CreateControl("$(parent)CalibrationDataAnchor", MapRadarContainer, CT_CONTROL)
+
+    dataAnchorControl:SetAnchor(LEFT, GuiRoot, LEFT, 100)
+    dataAnchorControl:SetDimensions(20, 100)
+
+    local line = CreateControl("$(parent)TestLine", MapRadarContainer, CT_LINE)
+    line:SetDimensions(20, 100)
+    line:SetAnchor(LEFT, dataAnchorControl, LEFT)
+
+    local widthLabel = CreateLabel(RIGHT, dataAnchorControl, LEFT, "Zone")
+    ScaleData.zoneName = CreateLabel(LEFT, dataAnchorControl, RIGHT, "")
+
+    local widthLabel = CreateLabel(TOPRIGHT, widthLabel, BOTTOMRIGHT, "Map Width")
+    ScaleData.mapWidth = CreateLabel(TOPLEFT, ScaleData.zoneName, BOTTOMLEFT, "0")
+
+    local heightLabel = CreateLabel(TOPRIGHT, widthLabel, BOTTOMRIGHT, "Map Height")
+    ScaleData.mapHeight = CreateLabel(TOPLEFT, ScaleData.mapWidth, BOTTOMLEFT, "0")
+
+    local curvCoomLabel = CreateLabel(TOPRIGHT, heightLabel, BOTTOMRIGHT, "Curv zoom")
+    ScaleData.curvedZoom = CreateLabel(TOPLEFT, ScaleData.mapHeight, BOTTOMLEFT, "0")
+
+    local reldxLabel = CreateLabel(TOPRIGHT, curvCoomLabel, BOTTOMRIGHT, "Rel DX")
+    ScaleData.rel_dx = CreateLabel(TOPLEFT, ScaleData.curvedZoom, BOTTOMLEFT, "0")
+
+    local reldyLabel = CreateLabel(TOPRIGHT, reldxLabel, BOTTOMRIGHT, "Rel DY")
+    ScaleData.rel_dy = CreateLabel(TOPLEFT, ScaleData.rel_dx, BOTTOMLEFT, "0")
+
+    local mapdxLabel = CreateLabel(TOPRIGHT, reldyLabel, BOTTOMRIGHT, "Map DX")
+    ScaleData.map_dx = CreateLabel(TOPLEFT, ScaleData.rel_dy, BOTTOMLEFT, "0")
+
+    local mapdyLabel = CreateLabel(TOPRIGHT, mapdxLabel, BOTTOMRIGHT, "Map DY")
+    ScaleData.map_dy = CreateLabel(TOPLEFT, ScaleData.map_dx, BOTTOMLEFT, "0")
+
+    local unit1Label = CreateLabel(TOPRIGHT, mapdyLabel, BOTTOMRIGHT, "Unit1")
+    ScaleData.unit1 = CreateLabel(TOPLEFT, ScaleData.map_dy, BOTTOMLEFT, "0")
+
+    local mrScaleLabel = CreateLabel(TOPRIGHT, unit1Label, BOTTOMRIGHT, "MR Scale")
+    ScaleData.mrScale = CreateLabel(TOPLEFT, ScaleData.unit1, BOTTOMLEFT, "0")
+
 end
 
 function MapRadar_InitScaleCalibrator(parent)
+
+    CreateCalibrationDataForm()
 
     local mgridTexture = CreateControl("$(parent)Mgrid", MapRadarContainer, CT_TEXTURE)
     mgridTexture:SetTexture("MapRadar/mgrid.dds")
@@ -21,7 +130,7 @@ function MapRadar_InitScaleCalibrator(parent)
     setScaleLabel(0)
 
     local btnAdd01 = CreateControlFromVirtual("$(parent)btnAdd01", MapRadarContainer, "plusButtonTemplate")
-    btnAdd01:SetAnchor(TOPLEFT, scaleLabel, TOPRIGHT)
+    btnAdd01:SetAnchor(TOPLEFT, scaleLabel, BOTTOMLEFT)
     btnAdd01:SetHandler("OnClicked", function()
         setScaleLabel(0.1)
     end)
@@ -56,4 +165,20 @@ function MapRadar_InitScaleCalibrator(parent)
         setScaleLabel(-0.001)
     end)
 
+    EVENT_MANAGER:RegisterForUpdate("MapRadar_PinReader", 100, function()
+        local pins = ZO_WorldMap_GetPinManager():GetActiveObjects()
+
+        for pinKey, pin in pairs(pins) do
+            -- MAP_PIN_TYPE_ACTIVE_COMPANION
+            -- MAP_PIN_TYPE_GROUP_LEADER
+
+            if pin:IsCompanion() -- pin:GetPinType() == MAP_PIN_TYPE_GROUP_LEADER
+            and pin.normalizedX and pin.normalizedY then
+                showCalibrationData(pin)
+                return
+            end
+        end
+
+    end)
 end
+

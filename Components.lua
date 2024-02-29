8731260c36baf871f8a7e956d3102662162525a8
Edgars Cizevskis <98318868+edgarscizevskis@users.noreply.github.com>
2024-02-29 14:36:11 +0200

    Update LabelStack fort its base object to be label control directly so object can be used in UI for anchoring and correct anchor calculation

--- Components.lua
+++ Components.lua
@@ -17,34 +17,36 @@ end
 -- Label stack
 local LabelStack = {}
 function LabelStack:New(name, parent, count)
-    local labelStack = {
-        name = name,
-        count = count,
-        control = CreateControl(name, parent, CT_CONTROL),
-        labels = {}
-    }
-    setmetatable(labelStack, self)
-    self.__index = self
+    local control = CreateLabel(name, parent, " ") -- Empty space forces to render tabel text and calc its bottom position for anchors
 
-    local label = CreateLabel("$(parent)placeholder", labelStack.control)
-    label:SetAnchor(TOPLEFT, labelStack.control, TOPLEFT)
-    label:SetAnchor(BOTTOMLEFT, labelStack.control, BOTTOMLEFT) -- maybe would be required to keep parent control height in check
+    control.name = name
+    control.count = count
+    control.labels = {}
+
+    local label = CreateLabel("$(parent)placeholder", control)
+    label:SetAnchor(TOPLEFT, control, TOPRIGHT)
 
     -- Create list of label controls here and anchor them together
     for i = 1, count do
-        local nextLabel = CreateLabel("$(parent)Label" .. i, labelStack.control)
+        local nextLabel = CreateLabel("$(parent)Label" .. i, control)
         nextLabel:SetAnchor(TOPLEFT, label, TOPRIGHT, 10)
-        labelStack.labels[i] = nextLabel
+        nextLabel:SetAnchor(BOTTOMLEFT, label, BOTTOMRIGHT, 10)
+        control.labels[i] = nextLabel
         label = nextLabel
     end
 
-    return labelStack
+    control.SetText = function(self, text)
+        -- puch all label texts further 
+        for i = self.count, 2, -1 do
+            self.labels[i]:SetText(self.labels[i - 1]:GetText())
+        end
+        self.labels[1]:SetText(text)
+    end
+
+    return control
 end
 
-function LabelStack:SetAnchor(...)
-    self.control:SetAnchor(...)
-end
-
+--[[
 function LabelStack:SetFont(...)
     -- maybe need to iterate child label components and set that
     self.control:SetFont(...)
@@ -55,17 +57,7 @@ function LabelStack:SetColor(...)
     self.control:SetColor(...)
 end
 
-function LabelStack:SetText(text)
-    -- puch all label texts further 
-    for i = self.count, 2, -1 do
-        self.labels[i]:SetText(self.labels[i - 1]:GetText())
-    end
-    self.labels[1]:SetText(text)
-end
-
-function LabelStack:getName()
-    return self.name
-end
+--]]
 
 -- namespace to export class to public
 MapRadarCommon = {
@@ -76,10 +68,7 @@ EVENT_MANAGER:RegisterForEvent("MapRadar", EVENT_PLAYER_ACTIVATED, function()
     local ls2 = MapRadarCommon.LabelStack:New("$(parent)Stacktest1", MapRadarContainer, 2)
     ls2:SetAnchor(TOP, GuiRoot, TOP, 0, 150)
     local ls5 = MapRadarCommon.LabelStack:New("$(parent)StackTest2", MapRadarContainer, 5)
-    ls5:SetAnchor(TOPLEFT, ls2.control, BOTTOMLEFT, 0, 30)
-
-    -- MapRadar.debug("C Name: <<1>>", MapRadar.getVal(ls2:getName()))
-    -- MapRadar.debug("C2 Name: <<1>>", MapRadar.getVal(ls5:getName()))
+    ls5:SetAnchor(TOPLEFT, ls2, BOTTOMLEFT)
 
     zo_callLater(function()
         ls2:SetText(123)
